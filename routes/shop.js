f9b1ec74d1030f72334207c6682e048f9d9d315d
Ankit Sharma <ankitsharma101299@gmail.com>
2021-07-23 15:18:36 +0530

    Add swagger first API to the docs

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // Imported Controller
-const shopController = require("../routes/shop");
+const shopController = require("../controllers/shop");
 
 // Setting router to handle different paths
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description : Use to request all products
+ *    responses:
+ *      '200':
+ *          description : A successful response containing all products */
 router.get("/", shopController.getIndex);
 
 module.exports = router;
