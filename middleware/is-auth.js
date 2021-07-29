f6854f466110b331597917ba838b49c3c2cc9d71
Ankit Sharma <ankitsharma101299@gmail.com>
2021-07-29 14:22:29 +0530

    Add error handling

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,28 +2,41 @@ const jwt = require("jsonwebtoken");
 const errorHandler = require("../controllers/error.handler");
 
 module.exports = (req, res, next) => {
-  const authHeader = req.get("Authorization").split(" ")[1];
-
-  if (!authHeader) {
-    const error = new Error("Kindly provide the Auth Header with the request");
-    throw errorHandler(error, 401);
-  }
-
-  const token = authHeader;
-  let decodedToken;
-
   try {
-    decodedToken = jwt.verify(token, "some@secret#");
-  } catch (err) {
-    err.statusCode = 500;
-    throw err;
-  }
+    if (!req.get("Authorization")) {
+      const error = new Error(
+        "Kindly provide the Auth Header with the request"
+      );
+      throw errorHandler(error, 401);
+    }
 
-  if (!decodedToken) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
+    const authHeader = req.get("Authorization").split(" ")[1];
+
+    if (!authHeader) {
+      const error = new Error(
+        "Kindly provide the Auth Header with the request"
+      );
+      throw errorHandler(error, 401);
+    }
+
+    const token = authHeader;
+    let decodedToken;
+
+    try {
+      decodedToken = jwt.verify(token, "some@secret#");
+    } catch (err) {
+      err.statusCode = 500;
+      throw err;
+    }
+
+    if (!decodedToken) {
+      const error = new Error("Not authenticated");
+      error.statusCode = 401;
+      throw error;
+    }
+    req.session.userId = decodedToken.userId;
+    next();
+  } catch (error) {
+    next(error);
   }
-  req.session.userId = decodedToken.userId;
-  next();
 };
