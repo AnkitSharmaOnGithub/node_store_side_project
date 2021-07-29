fa1795f2ee47d05b540b6414214629342a24ba26
Ankit Sharma <ankitsharma101299@gmail.com>
2021-07-29 14:22:22 +0530

    Add error handling

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,30 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
 const isAuth = require("../middleware/is-auth");
+const { body } = require("express-validator");
 
-router.post("/signUp", userController.signUp);
+router.post(
+  "/signUp",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Please enter a valid email address"),
+    body("password")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Please enter a password atleast 5 characters long"),
+  ],
+  userController.signUp
+);
 
-router.post("/signIn", userController.signIn);
+router.post(
+  "/signIn",
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+  userController.signIn
+);
 
 module.exports = router;
