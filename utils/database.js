9a0fb60dae049ddb798e79c980f2fb2e7292750f
Ankit Sharma <ankitsharma101299@gmail.com>
2021-08-03 22:48:09 +0530

    Add enviroment variables

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("node_store_side_project", "root", "", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, "", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
